eef6d4ffb3296096848d1a8570e1f8e76f1e9f98
Thomas <gudinthomas@gmail.com>
2019-03-11 19:30:50 -0400

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+import{Pokemon} from './pokemon';
+//import {POKEMONS} from './mock-pokemons';
+
+@Component({
+  selector: 'pokemon-app',
+  templateUrl: `./app/app.component.html`,
+})
+export class AppComponent implements OnInit {
+
+  private pokemons: Pokemon[] = null;
+
+
+ }
