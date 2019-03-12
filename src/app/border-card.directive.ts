4796f3c4a3c266dbf48d9fa332dcae48aa7cde93
Thomas <gudinthomas@gmail.com>
2019-03-12 16:43:51 -0400

    alias

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,27 +1,30 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+	import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
-@Directive({
-  selector: '[pkmnBorderCard]'
-})
-export class BorderCardDirective {
-	constructor(private el: ElementRef) {
-		this.setBorder('#f5f5f5');
-		this.setHeight(180);
+	@Directive({
+		selector: '[pkmnBorderCard]'
+	})
+	export class BorderCardDirective {
+		constructor(private el: ElementRef) {
+			this.setBorder('#f5f5f5');
+			this.setHeight(180);
+		}
+
+	@HostListener('mouseenter') onMouseEnter(){
+		this.setBorder(this.borderColor || '#009688')
+	}
+	@HostListener('mouseleave') onMouseleave(){
+		this.setBorder('#f5f5f5')
 	}
 
-@HostListener('mouseenter') onMouseEnter(){
-	this.setBorder('#009688')
-}
-@HostListener('mouseleave') onMouseleave(){
-	this.setBorder('#f5f5f5')
-}
+	@Input('pkmnBorderCard') borderColor:string; //alias
+//	@Input() pkmnBorderCard:string: // sans alias
 
-	private setBorder(color: string) {
-		let border = 'solid 4px ' + color;
-		this.el.nativeElement.style.border = border;
-	}
+		private setBorder(color: string) {
+			let border = 'solid 4px ' + color;
+			this.el.nativeElement.style.border = border;
+		}
 
-	private setHeight(height: number) {
-		this.el.nativeElement.style.height = height + 'px';
+		private setHeight(height: number) {
+			this.el.nativeElement.style.height = height + 'px';
+		}
 	}
-}
