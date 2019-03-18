0bae3390b099f66727191627d4e31ce6bb2788a3
Thomas <gudinthomas@gmail.com>
2019-03-18 11:45:30 -0400

    Routes + 404

---
+++ src/app/page-not-found.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'page-404',
+	templateUrl:'./app/vues/page-not-found.component.html'
+})
+export class PageNotFoundComponent { }
